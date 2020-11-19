3eaaf28d9ed827b39dcb9bf1a3f557cbc0ef6ecd
John Ryan <john.p.ryan4@gmail.com>
2020-11-19 07:34:47 -0800

    add library and test files

--- lib/page_router.dart
+++ lib/page_router.dart
@@ -1,1 +1,152 @@
 library page_router;
+
+import 'package:flutter/material.dart';
+import 'package:trie_router/trie_router.dart';
+import 'package:path/path.dart' as path;
+
+class PageRouter extends InheritedWidget {
+  final PageRouterData data;
+
+  const PageRouter({
+    Key key,
+    @required this.data,
+    @required Widget child,
+  }) : super(key: key, child: child);
+
+  static PageRouter of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<PageRouter>();
+  }
+
+  void pushNamed(String routeName) {
+    data.pushNamed(routeName);
+  }
+
+  @override
+  bool updateShouldNotify(PageRouter old) => data != old.data;
+}
+
+typedef PageRouterBuilder = Page Function(
+    BuildContext context, Map<String, String> parameters);
+
+class PageRouterData {
+  final TrieRouter<PageRouterBuilder> _trieRouter;
+  final PageRouterDelegate routerDelegate;
+  final PageRouterInformationParser informationParser;
+
+  PageRouterData._(this._trieRouter)
+      : routerDelegate = PageRouterDelegate(_trieRouter),
+        informationParser = PageRouterInformationParser(_trieRouter);
+
+  factory PageRouterData(Map<String, PageRouterBuilder> routes) =>
+      PageRouterData._(_createTrieRouter(routes));
+
+  static TrieRouter<PageRouterBuilder> _createTrieRouter(
+      Map<String, PageRouterBuilder> routes) {
+    var trie = TrieRouter<PageRouterBuilder>();
+    for (var key in routes.keys) {
+      trie.add(path.split(key), routes[key]);
+    }
+    return trie;
+  }
+
+  void pushNamed(String routeName) {
+    routerDelegate.pushNamed(routeName);
+  }
+}
+
+class _RouteData {
+  // The pattern used to parse the route string. e.g. "/books/:id"
+  final String routeString;
+
+  _RouteData(this.routeString);
+
+  @override
+  bool operator ==(Object other) {
+    return other is _RouteData && routeString == other.routeString;
+  }
+
+  int get hashCode {
+    routeString.hashCode;
+  }
+
+  String toString() {
+    return 'RooterData route: ${routeString}';
+  }
+}
+
+class PageRouterDelegate extends RouterDelegate<_RouteData>
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin<_RouteData> {
+  final TrieRouter<PageRouterBuilder> trie;
+
+  @override
+  final GlobalKey<NavigatorState> navigatorKey;
+
+  List<String> _routeStack = ['/'];
+
+  PageRouterDelegate(this.trie) : navigatorKey = GlobalKey<NavigatorState>();
+
+  _RouteData get currentConfiguration {
+    return _RouteData(_routeStack.last);
+  }
+
+  void pushNamed(String name) {
+    _routeStack.add(name);
+    notifyListeners();
+  }
+
+  void pop() {
+    _routeStack.removeLast();
+    notifyListeners();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      key: navigatorKey,
+      pages: [
+        // TODO: give the user the option to customize the page (cupertino or
+        // material)
+        ..._routeStack.map((routeName) {
+          var trieData = trie.get(path.split(routeName));
+          return trieData.value(context, trieData.parameters);
+        }),
+      ],
+      onPopPage: (route, result) {
+        if (!route.didPop(result)) {
+          return false;
+        }
+
+        _routeStack.removeLast();
+        notifyListeners();
+        return true;
+      },
+    );
+  }
+
+  @override
+  Future<void> setNewRoutePath(_RouteData configuration) {
+    // TODO: allow parent pages to be included
+    // Currently, this clears the route stack, which means
+    // Any time a new deep link is handled the set of pages is cleared.
+    _routeStack
+      ..clear()
+      ..add(configuration.routeString);
+  }
+}
+
+class PageRouterInformationParser extends RouteInformationParser<_RouteData> {
+  final TrieRouter trie;
+
+  PageRouterInformationParser(this.trie);
+
+  @override
+  Future<_RouteData> parseRouteInformation(
+      RouteInformation routeInformation) async {
+    return _RouteData(routeInformation.location);
+  }
+
+  @override
+  RouteInformation restoreRouteInformation(_RouteData data) {
+    return RouteInformation(location: data.routeString);
+  }
+}
---
+++ test/page_router_test.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:page_router/page_router.dart';
+
+void main() {
+  testWidgets('basic test', (tester) async {
+    var pageRouter = PageRouterData({
+      '/': (context, params) => MaterialPage(
+        child: Scaffold(
+          body: Center(
+            child: Text('home screen'),
+          ),
+        ),
+      ),
+      '/details': (context, params) => MaterialPage(
+        child: Scaffold(
+          body: Center(
+            child: Text('details screen'),
+          ),
+        ),
+      ),
+      '/user/:id': (context, params) => MaterialPage(
+        child: Scaffold(
+          body: Center(
+            child: Text('User ${params[":id"]}'),
+          ),
+        ),
+      ),
+    });
+    await tester.pumpWidget(_TestApp(pageRouter));
+    expect(find.text('home screen'), findsOneWidget);
+    pageRouter.pushNamed('/details');
+    await tester.pumpAndSettle();
+    expect(find.text('details screen'), findsOneWidget);
+    pageRouter.pushNamed('/user/123');
+    await tester.pumpAndSettle();
+    expect(find.text('User 123'), findsOneWidget);
+  });
+}
+
+class _TestApp extends StatefulWidget {
+  final PageRouterData pageRouterData;
+
+  _TestApp(this.pageRouterData);
+
+  @override
+  __TestAppState createState() => __TestAppState();
+}
+
+class __TestAppState extends State<_TestApp> {
+  @override
+  Widget build(BuildContext context) {
+    return PageRouter(
+      data: widget.pageRouterData,
+      child: MaterialApp.router(
+        title: 'Books App',
+        routerDelegate: widget.pageRouterData.routerDelegate,
+        routeInformationParser: widget.pageRouterData.informationParser,
+      ),
+    );
+  }
+}
